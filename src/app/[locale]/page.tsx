baf3a94718cd79588f3fc002ebb64d31fdc180fc
cramanan <cramananjaonapro@gmail.com>
2024-09-14 14:21:23 +0200

    feat: added i18n

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@/i18n/routing";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Link href="/tech-stack">see more</Link>
+        </div>
+    );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export function Navbar() {
+    return (
+        <nav>
+            <h1>Cyril Ram.</h1>
+            <div id="flags">
+                <Link href=".">lmao</Link>
+            </div>
+        </nav>
+    );
+}
+
+export function Footer() {
+    return <footer></footer>;
+}
