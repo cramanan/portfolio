2c8d5daa8bdcabec572933809f328a72c5f5f733
cramanan <cramananjaonapro@gmail.com>
2024-09-16 10:38:59 +0200

    feat: added tech-stack

--- src/app/[locale]/tech-stack/page.tsx
+++ src/app/[locale]/tech-stack/page.tsx
@@ -1,8 +1,74 @@
 import { useTranslations } from "next-intl";
 import React from "react";
-import "./Stack.scss";
+import Image from "next/image";
 
-const mock = [1, 2, 3, 4, 5, 6];
+import "./Stack.scss";
+import nextLogo from "../../assets/svgs/next.svg";
+import vercelLogo from "../../assets/svgs/vercel.svg";
+import htmlLogo from "../../assets/svgs/html.svg";
+import cssLogo from "../../assets/svgs/css.svg";
+import jsLogo from "../../assets/svgs/javascript.svg";
+import pythonLogo from "../../assets/svgs/python.svg";
+import sqliteLogo from "../../assets/svgs/sqlite.svg";
+import typescriptLogo from "../../assets/svgs/typescript.svg";
+import reactLogo from "../../assets/svgs/react.svg";
+import gitLogo from "../../assets/svgs/git.svg";
+import dockerLogo from "../../assets/svgs/docker.svg";
+import golangLogo from "../../assets/svgs/golang.svg";
+import postgresLogo from "../../assets/svgs/postgres.svg";
+
+type Props = {
+    title: string;
+    path: string;
+}[];
+
+const elements: Props[] = [
+    [
+        { title: "Next.js", path: nextLogo },
+        { title: "Vercel", path: vercelLogo },
+    ],
+    [
+        { title: "Git", path: gitLogo },
+        { title: "Docker", path: postgresLogo },
+    ],
+    [
+        { title: "Golang", path: golangLogo },
+        { title: "PostgreSQL", path: dockerLogo },
+    ],
+    [
+        { title: "React", path: reactLogo },
+        { title: "TypeScript", path: typescriptLogo },
+    ],
+    [
+        { title: "Python", path: pythonLogo },
+        { title: "SQLite", path: sqliteLogo },
+    ],
+    [
+        { title: "HTML", path: htmlLogo },
+        { title: "CSS", path: cssLogo },
+        { title: "JS", path: jsLogo },
+    ],
+];
+
+function StackElement({ props }: { props: Props }) {
+    return (
+        <li>
+            <div className="stack-element">
+                {props.map(({ title, path }) => (
+                    <div key={title} className="tech">
+                        <span>{title}</span>
+                        <Image
+                            src={path}
+                            height={50}
+                            width={50}
+                            alt={`${title} logo`}
+                        />
+                    </div>
+                ))}
+            </div>
+        </li>
+    );
+}
 
 export default function TechStack() {
     const t = useTranslations("Tech-Stack");
@@ -12,11 +78,10 @@ export default function TechStack() {
             <div id="stack">
                 <h3>{t("learning")}</h3>
                 <ul>
-                    {mock.map((_, index) => (
-                        <li key={index}>{index}</li>
+                    {elements.map((elt, idx) => (
+                        <StackElement key={idx} props={elt} />
                     ))}
                 </ul>
-                <h3>{t("basics")}</h3>
             </div>
         </>
     );
