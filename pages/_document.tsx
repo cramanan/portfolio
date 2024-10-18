1678709bbd23bd6a19a67ed8ce3a03486ef50be4
cramanan <cramananjaonapro@gmail.com>
2024-10-18 15:49:58 +0200

    feat: added keywords

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html lang="fr" translate="no">
+        <Html translate="no" lang="fr">
             <Head />
             <body>
                 <Main />
